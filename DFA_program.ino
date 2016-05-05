eaf9422af4af8bf9b88ee701bc87de91a232e7a5
letaureau <parthranjansingh@gmail.com>
2016-05-05 15:42:30 -0400

    Updated with general

--- DFA_program.ino
+++ DFA_program.ino
@@ -1,19 +1,3 @@
-/*
-
-Voice Recognition Shield Example
-
-This example shows an application on 1Sheeld's voice recognition shield.
-
-By using this example, you can play, delay and stop your smartphone's
-music using voice commands.
-
-OPTIONAL:
-To reduce the library compiled size and limit its memory usage, you
-can specify which shields you want to include in your sketch by
-defining CUSTOM_SETTINGS and the shields respective INCLUDE_ define.
-
-*/
-
 #define CUSTOM_SETTINGS
 #define INCLUDE_VOICE_RECOGNIZER_SHIELD
 #define INCLUDE_MUSIC_PLAYER_SHIELD
@@ -21,8 +5,10 @@ defining CUSTOM_SETTINGS and the shields respective INCLUDE_ define.
 #define INCLUDE_TERMINAL_SHIELD
 #define INCLUDE_NFC_SHIELD
 
-
 /* Include 1Sheeld library. */
+#include "rgb_lcd.h"
+#include <Wire.h>
+
 #include <OneSheeld.h>
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
@@ -41,40 +27,48 @@ defining CUSTOM_SETTINGS and the shields respective INCLUDE_ define.
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+rgb_lcd lcd;
 const int vib = 3;
 const int light = 2;
 int flag = 0;
 /* Voice commands set by the user. */
-const char * personality1 = "parth";
-const char * personality2 = "namita";
-const char * personality3 = "izabela";
-const char musicCommand[] = "music";
-const char lightCommand[] = "lights";
-const char vibrationCommand[] = "vibration";
-const char audiobookCommand[] = "audiobook";
+const char personality1 = "parth";
+const char personality2 = "namita";
+const char personality3 = "izabela";
+const char personality;
+const char musicCommand1[] = "music";
+const char musicCommand2[] = "songs";
+const char lightCommand1[] = "lights";
+const char lightCommand2[] = "light";
+const char vibrationCommand1[] = "vibration";
+const char vibrationCommand2[] = "vibrations";
+const char welcomeCommand[] = "hello dexter";
 const char stopCommand[] = "stop";
 
 
 void setup()
 {
+
+  pinMode(vib, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.print("Welcome!!!");
+  lcd.setRGB(255, 0, 0);
+  delay(1000);
+  lcd.setRGB(0, 0, 255);
+  delay(1000);
+  lcd.setRGB(0, 255, 0);
+  delay(1000);
   /* Start Communication. */
   OneSheeld.begin();
   /* Error Commands handiling. */
+  VoiceRecognition.setOnError(error);
   /* Subscribe to the new tag scanned event. */
   NFC.setOnNewTagScanned(&tagFunction);
   /* Subscribe to on Tag error event. */
   NFC.setOnError(&tagErrorFunction);
-  /* Error Voice Recognition Commands handiling. */
-  VoiceRecognition.setOnError(error);
-  TextToSpeech.say("I AM DEXTER YOUR PERSONAL SAFETY ASSISTANT");
-  delay(5000);
-  TextToSpeech.say("Good afternoon, I see that you have not slept well the day before, which of the settings would you like me to enable");
-  delay(10000);
-  TextToSpeech.say("Your options are : Vibration, Lights, Music and Audiobook");
-  delay(5000);
   pixels.begin(); // This initializes the NeoPixel library.
   VoiceRecognition.start();
-
+  delay(3000);
 }
 
 void loop ()
@@ -82,41 +76,40 @@ void loop ()
   /* Check if new command received. */
   if (VoiceRecognition.isNewCommandReceived())
   {
-    /* Compare the play command. */
-    if (!strcmp(musicCommand, VoiceRecognition.getLastCommand()))
+    lcd.clear();
+    lcd.print(VoiceRecognition.getLastCommand());
+    delay(2000);
+    /* Compare the music command. */
+    if (!strcmp(musicCommand1, VoiceRecognition.getLastCommand()) || !strcmp(musicCommand2, VoiceRecognition.getLastCommand()))
     {
+      stopping();
+      lcd.clear();
+      lcd.print("MUSIC ON");
       TextToSpeech.say("MUSIC ON");
       delay(2000);
-      //if(flag = 0)
-      //{
-      /* MUSIC */
-      MusicPlayer.next();
-      //}
-      //else if(flag =1)
-      //{
-      //  MusicPlayer.play();
-      //}
-      //flag =0;
+      MusicPlayer.play();
     }
-    /* Compare the delay command. */
-    else if (!strcmp(lightCommand, VoiceRecognition.getLastCommand()))
+    /* Compare the lights command. */
+    else if (!strcmp(lightCommand1, VoiceRecognition.getLastCommand()) || !strcmp(lightCommand2, VoiceRecognition.getLastCommand()))
     {
+      stopping();
+      lcd.clear();
+      lcd.print("LIGHTS ON");
       TextToSpeech.say("LIGHTS ON");
       delay(2000);
       /* LIGHT. */
-      for (int i = 0; i < NUMPIXELS; i++) {
-
+      for (int i = 0; i < NUMPIXELS; i++)
+      {
         // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
         pixels.setPixelColor(i, pixels.Color(0, 0, 255)); // Moderately bright green color.
-
         pixels.show(); // This sends the updated pixel color to the hardware.
-
-
       }
-
     }
-    else if (!strcmp(vibrationCommand, VoiceRecognition.getLastCommand()))
+    else if (!strcmp(vibrationCommand1, VoiceRecognition.getLastCommand()) || !strcmp(vibrationCommand2, VoiceRecognition.getLastCommand()))
     {
+      stopping();
+      lcd.clear();
+      lcd.print("VIBRATION ON");
       TextToSpeech.say("VIBRATION ON");
       delay(2000);
       /* VIBRATION. */
@@ -133,27 +126,12 @@ void loop ()
     {
       TextToSpeech.say("STOP");
       delay(2000);
-      /* Stop the track. */
-      MusicPlayer.stop();
-      digitalWrite(vib, LOW);
-      for (int i = 0; i < NUMPIXELS; i++) {
-
-        // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-        pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
-
-        pixels.show(); // This sends the updated pixel color to the hardware.
-
-
-      }
+      stopping();
     }
     /* Compare the next command. */
-    else if (!strcmp(audiobookCommand, VoiceRecognition.getLastCommand()))
+    else if (!strcmp(welcomeCommand, VoiceRecognition.getLastCommand()))
     {
-      TextToSpeech.say("AUDIOBOOK ON");
-      delay(2000);
-      /* Next track. */
-      MusicPlayer.next();
-      flag = 1;
+      welcome();
     }
   }
 }
@@ -173,3 +151,123 @@ void error(byte errorData)
     case RECOGNIZER_BUSY_ERROR: Terminal.println("Busy"); break;
   }
 }
+
+/* OnError event. */
+void tagErrorFunction(byte errorNumber)
+{
+  /* Switch on error number. */
+  switch(errorNumber)
+  {
+    case INDEX_OUT_OF_BOUNDS: Terminal.println("INDEX_OUT_OF_BOUNDS");break;
+    case RECORD_CAN_NOT_BE_PARSED: Terminal.println("RECORD_CAN_NOT_BE_PARSED");break;
+    case TAG_NOT_SUPPORTED: Terminal.println("TAG_NOT_SUPPORTED");break;
+    case NO_ENOUGH_BYTES: Terminal.println("NO_ENOUGH_BYTES");break;
+    case TAG_READING_ERROR: Terminal.println("TAG_READING_ERROR");break;
+    case RECORD_NOT_FOUND: Terminal.println("RECORD_NOT_FOUND");break;
+  }
+}
+
+
+void welcome()
+{
+  stopping();
+  lcd.clear();
+  lcd.print("TIME SLEPT:");
+  lcd.setCursor(2, 1);
+  lcd.print("5:35:16");
+  delay(1000);
+  TextToSpeech.say("I AM DEXTER YOUR PERSONAL SAFETY ASSISTANT");
+  delay(3000);
+  TextToSpeech.say("Good morning");
+  TextToSpeech.say(personality);
+  TextToSpeech.say("I see that you have not slept well the day before, which of the settings would you like me to enable");
+  delay(5000);
+  TextToSpeech.say("Your options are : Vibration");
+  delay(2000);
+  digitalWrite(vib, HIGH);
+  delay(2000);
+  digitalWrite(vib, LOW);
+  delay(2000);
+  TextToSpeech.say("Lights");
+  rainbow(2000);
+  TextToSpeech.say("Music");
+  MusicPlayer.play();
+  delay(2000);
+  MusicPlayer.stop();
+  VoiceRecognition.start();
+  //delay(2000);
+}
+
+void responseFunction(byte recordNumber , char data[])
+{
+  /* Check response and compare data. */
+  if(!strcmp(data,personality1))
+  {
+    Terminal.println("Parth");
+    personality = "Parth";
+  }
+  else if(!strcmp(data,personality2))
+  {
+    Terminal.println("Namita");
+    personality = "Namita";
+  }
+  else if(!strcmp(data,personality3))
+  {
+    Terminal.println("Izabela");
+    personality = "Izabela";
+  }
+}
+
+void rainbow(int wait) {
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    int r = random(0, 255);
+    int g = random(0, 255);
+    int b = random(0, 255);
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(r, g, b)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+  delay(wait);
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+}
+
+void stopping()
+{
+  /* Stop the track. */
+  MusicPlayer.stop();
+  digitalWrite(vib, LOW);
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+}
+
+/* A function to be called once a new tag is scanned. */
+void tagFunction(NFCTag &myTag)
+{
+  /* Create and object of type NFCRecord. */
+  NFCRecord &myRecordNumber0 = myTag.getRecord(0);
+  /* Subscribe to record parsed data response event. */
+  myTag.setOnRecordParsedDataResponse(&responseFunction);
+  /* 
+  /* Check if there's a record with number 0 in Tag. */
+  if(!myRecordNumber0.isNull())
+  {
+    /* Parse and query the data in the first record. */
+    myRecordNumber0.queryParsedData();  
+  }
+  else
+  {
+    /* Print out no record found. */
+    Terminal.println("No Record found"); 
+  }
+}
+
